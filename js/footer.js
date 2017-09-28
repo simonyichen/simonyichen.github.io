b6d9210c8c96229fbaa2ed982dc9f5a870a86f26
simonyichen <simonc1@163.com>
2017-09-28 21:43:20 +0800

    copy from weihuix

---
+++ js/footer.js
@@ -0,0 +1,18 @@
+
+/*footer*/
+
+document.write("<div>");
+    document.write("<ul class=\"footer_menu_list\">");
+        document.write("<li>友情链接：</li>");
+        document.write("<li><a href=\"http://www.ustc.edu.cn\"  target=\"_blank\">中国科大</a></li>");
+        document.write("<li><a href=\"http://www.arcsoft.com.cn/\" target=\"_blank\">虹软中国</a></li>");
+        document.write("<li><a href=\"https://github.com/\"  target=\"_blank\">GitHub</a></li>");
+		document.write("<li><a href=\"http://www.amazingcounters.com\" target=\"_blank\"><img border=\"0\" src=\"http://cc.amazingcounters.com/counter.php?i=3179596&c=9539101\" alt=\"AmazingCounters.com\"></a></li>");			
+    document.write("</ul>");
+document.write("</div>");
+document.write("<div class=\"copyright\">");
+    document.write("Copyright © 2014-2015 BY Wenhuix | Email:<a href=\"mailto:littlewest@foxmail.com\" class=\"email\">littlewest@foxmail.com</a>");
+document.write("</div>");
+
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F6b5f4352b64ebec3e9a0e16c1e6c19e6' type='text/javascript'%3E%3C/script%3E"));
